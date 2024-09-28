c26cbac73dcca6571e627770e38bbd50c444634c
CAD User <CAD User@VM-898623>
2024-09-28 15:59:39 -0400

    created an arm

--- Fidgetspinnerjonathan.groovy
+++ Fidgetspinnerjonathan.groovy
@@ -1,1 +1,16 @@
-CSG simpleSyntax =new Cylinder(10,40).toCSG() // a one line Cylinder
+CSG simpleSyntax =new Cylinder(28/2,7).toCSG() // a one line Cylinder
+CSG hardSyntax =new Cylinder(22.15/2,7).toCSG()
+
+CSG mediumSyntax =simpleSyntax.movey(40)
+CSG modeSyntax =hardSyntax.movey(40)
+
+CSG hugeSyntax =new Cube(22,40,5).toCSG()
+					.toZMin()
+					.toYMin()
+
+CSG printedSyntax = simpleSyntax.union(hugeSyntax)
+							.union(mediumSyntax)
+printedSyntax = printedSyntax.difference(hardSyntax)
+							.difference(modeSyntax)
+
+return [printedSyntax]
